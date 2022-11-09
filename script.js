4037ec161a8877db00e0e2a9d282e8a2f3e049a7
RCMatthias <matthias.vdvelde@hotmail.com>
2022-11-09 15:10:29 +0100

    add initial functionality to js

---
+++ script.js
@@ -0,0 +1,47 @@
+document.getElementById("1").addEventListener("click", numbers.push(1) )
+
+const numbers = []
+
+const pushNum = () => {
+    displayVar.push["1"];
+}
+
+
+
+
+
+//Operate function: takes an operator and 2 numbers and then calls one of 
+//the math functions
+function operate (numOne, func, numTwo){
+    if (func === add) {
+
+    }
+}
+
+//display value variable
+let displayVar = [];
+
+let btn = getElementById("1") 
+
+btn.addEventListener("click", displayVar.push(1))
+
+//Basic math operators
+function add (numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+
+function subtract (numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+
+function multiply (numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+
+function divide (numOne, numTwo) {
+    return numOne / numTwo;
+}
+
